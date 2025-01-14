edb81ac8791b9dd5172e5dcb8cb751f8545a7fd0
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2025-01-14 16:26:33 +0000

    DDCE-6142

--- app/controllers/SignOutConfirmationController.scala
+++ app/controllers/SignOutConfirmationController.scala
@@ -16,7 +16,6 @@
 
 package controllers
 
-import config.FrontendAppConfig
 import javax.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
