eaf48871b6c26e7f7a118eb8ff6c30600a879c96
Mithra Balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-12-23 10:37:50 +0000

    DDCE-7780 - enabling service navigation (#137)
    * DDCE-7780 - enabling service navigation
    
    * DDCE-7780 - pr bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "10.4.0"
-  val mongoVersion = "2.10.0"
+  val boostrapVersion = "10.5.0"
+  val mongoVersion = "2.11.0"
 
   private lazy val compile = Seq(
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % mongoVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.20.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.25.0",
     "uk.gov.hmrc"             %% "domain-play-30"                         % "11.0.0",
-    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.3.0",
+    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % boostrapVersion,
     "uk.gov.hmrc"             %% "tax-year"                               % "6.0.0"
   )
