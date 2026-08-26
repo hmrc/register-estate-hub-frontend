4a157b74781661eb5457a6a941859e485faac255
harish-korada1 <174133905+harish-korada1@users.noreply.github.com>
2026-08-26 16:06:45 +0100

    DDCE:8671 : UI fix

--- app/controllers/HaveUTRYesNoController.scala
+++ app/controllers/HaveUTRYesNoController.scala
@@ -55,14 +55,18 @@ class HaveUTRYesNoController @Inject() (
 
   def onPageLoad(origin: Option[String]): Action[AnyContent] = actions() { implicit request =>
     val preparedForm = request.userAnswers.get(HaveUTRYesNoPage) match {
-      case None        => form
+      case None        =>
+        form
       case Some(value) =>
         // UCD request : we need to show no in UI when user clicks back btn
         // from are you sure page, this only happens if the query param is present in url
-        if (origin == queryParmaValue)
-          form.fill(false)
-        else
-          form.fill(value)
+        origin match {
+          case Some(originValue) if originValue == queryParmaValue =>
+            form.fill(false)
+          case _ =>
+            form.fill(value)
+        }
+
     }
 
     Ok(view(preparedForm, isOrgCredUser, origin))
